0f2239c1fa711c97597d465be7a18ef9212abc3b
radhad-ux <radhad83@gmail.com>
2022-04-12 20:27:02 +0200

    navigation bar added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router-dom";
+
+import Logo from "../assets/barbecue.png";
+
 export default function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <nav className="navbar_links">
+      <Link to="/">
+        <img className="navbar_img" src={Logo} alt="" />
+      </Link>
+      <h3 className="navbar_title">Firehouse BBQ</h3>
+      <div className="spacer"></div>
+      <Link to="/">Home</Link>
+      <Link to="/menu">Menu</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
 }
